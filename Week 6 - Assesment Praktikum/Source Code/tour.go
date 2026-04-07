9db12d0622832efc651a5e4d34758cdf37493e6d
danar-code <danaralkausar.telyupwt@gmail.com>
2026-04-07 16:39:04 +0700

    Update Assesment Laprak

---
+++ Week 6 - Assesment Praktikum/Source Code/tour.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func tanggunganHari(jumlahHari int, tujuan string) int {
+	if tujuan == "domestik" {
+		if jumlahHari > 3 {
+			return 3
+		}
+		return jumlahHari
+	} else {
+		if jumlahHari > 8 {
+			return 8
+		}
+		return jumlahHari
+	}
+}
+
+func biayaPerHari(jumlahMhs int) int {
+	// biaya per hari domestik
+	biayaMakan := 2 * 35000
+	biayaPenginapan := 250000
+	uangSaku := 300000
+
+	totalPerHari := biayaMakan + biayaPenginapan + uangSaku
+
+	return totalPerHari * jumlahMhs
+}
+
+func perhitunganBiaya(jumlahMhs, lamaPerjalanan int, tujuan string, totalBiaya *float64) {
+	hari := tanggunganHari(lamaPerjalanan, tujuan)
+
+	biayaHarian := biayaPerHari(jumlahMhs)
+
+	if tujuan == "domestik" {
+		*totalBiaya = float64(hari * biayaHarian)
+	} else {
+		*totalBiaya = float64(hari*biayaHarian) * 1.5
+	}
+}
+
+func main() {
+	var jumlah, lama int
+	var tujuan string
+	var biaya float64
+
+	fmt.Print("Masukkan jumlah mahasiswa : ")
+	fmt.Scan(&jumlah)
+
+	fmt.Print("Masukkan lama hari study tour : ")
+	fmt.Scan(&lama)
+
+	fmt.Print("Masukkan tujuan study tour (domestik/mancanegara) : ")
+	fmt.Scan(&tujuan)
+
+	perhitunganBiaya(jumlah, lama, tujuan, &biaya)
+
+	fmt.Printf("Biaya perjalanan yang harus dikeluarkan Tel-U : Rp. %.0f\n", biaya)
+}
